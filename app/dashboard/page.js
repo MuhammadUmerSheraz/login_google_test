2223d6b3da12ef52bf17db08b81f24096ef270d5
Umer Sheraz <muhammadumersheraz@gmail.com>
2025-07-31 21:46:04 +0500

    Push

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -11,12 +11,26 @@ export default function DashboardPage() {
     <div style={{ textAlign: "center", marginTop: 100 }}>
       <h2>Welcome, {session.user.name}</h2>
       <p>Email: {session.user.email}</p>
-      <img src={session.user.image} alt="profile" width={100} height={100} />
       <br />
       <button onClick={() => signOut()} style={{ marginTop: 20 }}>
         Logout
       </button>
+
+      <pre
+        style={{
+          marginTop: 30,
+          background: "#f5f5f5",
+          padding: "20px",
+          textAlign: "left",
+          maxWidth: 600,
+          margin: "30px auto",
+          borderRadius: "8px",
+        }}
+      >
+        <code>{JSON.stringify(session, null, 2)}</code>
+      </pre>
     </div>
   );
+
 
 }
